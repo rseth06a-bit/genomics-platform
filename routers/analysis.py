aceeb57cb8e2267d43a0da51d48bcd101de64311
rseth06a <rseth06a@gmail.com>
2026-04-07 13:19:19 -0700

    analysis.py completed and tested

--- routers/analysis.py
+++ routers/analysis.py
@@ -1,7 +1,25 @@
 from fastapi import APIRouter
+from pipeline.features import extract_features
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Sample,Sequence
 
 router = APIRouter()
 
 @router.post("/analyze/{sample_id}")
-def analyze(sample_id: int):
-    return {"message": f"Analysis for sample {sample_id} not yet implemented"}
+def analyze(sample_id: int, db: Session=Depends(get_db)):
+    sample = db.query(Sample).filter(Sample.id == sample_id).first()
+    if not sample:
+        raise HTTPException(status_code=404, detail="Sample not found")
+    sequences = db.query(Sequence).filter(Sequence.sample_id == sample_id).all()
+    counter=0
+    for sequence in sequences:
+        features = extract_features(sequence.raw_sequence)
+        sequence.gc_content = features["gc_content"]
+        sequence.seq_length = features["seq_length"]
+        sequence.kmer_json = features["kmer_json"]
+        counter+=1
+    db.commit()
+    return(counter)
+
